a0e55a340d21dbc789c96da97edb979e375bfc20
ninefox <unvariant.winter@gmail.com>
2025-07-02 23:04:03 -0400

    add new method of scanning for files

--- pwnc/commands/kernel/compress.py
+++ pwnc/commands/kernel/compress.py
@@ -2,18 +2,32 @@ from pwnc.config import load_config
 from ...util import *
 import gzip
 
+
 def do_compress(rootfs: Path, destination: Path, gzipped: bool, gzip_level: int = 0):
-    files = rootfs.glob("**/*")
+    files = []
+    for path, dirlist, filelist in os.walk(rootfs):
+        path = Path(path)
+        files.extend([path / name for name in dirlist])
+        files.extend([path / name for name in filelist])
+    # files = rootfs.glob("**")
     files = map(lambda p: p.relative_to(rootfs), files)
     files = list(files)
+    # print(files)
     delimited = b"\x00".join(map(lambda file: str(file).encode(), files))
-    handle = run("cpio --null -o --format=newc --owner=root", input=delimited, capture_output=True, encoding=None, cwd=rootfs)
+    handle = run(
+        "cpio --null -o --format=newc --owner=root",
+        input=delimited,
+        capture_output=True,
+        encoding=None,
+        cwd=rootfs,
+    )
     archive = handle.stdout
     if gzipped:
         archive = gzip.compress(archive, gzip_level)
 
     with open(destination, "wb+") as fp:
         fp.write(archive)
+
 
 def command(args):
     config_initramfs = config.Key("kernel") / "initramfs"
